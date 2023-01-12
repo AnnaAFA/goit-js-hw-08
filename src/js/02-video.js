b1f7ec434e4817722da472989f69f012c9236af1
AnnaAFA <anna.fedoruk111@gmail.com>
2023-01-12 15:10:27 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+// console.log(Player);
+const throttle = require('lodash.throttle');
+// console.log(throttle);
+const iframe = document.querySelector('iframe');
+// console.log(iframe);
+const player = new Player(iframe);
+// console.log(player);
+player.on('timeupdate', throttle(onPlay, 1000));
 
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+(function onPlayerPlay() {
+  const saveData = localStorage.getItem('videoplayer-current-time');
+  if (!saveData) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time') ?? 0);
+})();
